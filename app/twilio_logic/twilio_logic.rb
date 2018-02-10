8cf0b50f2d4c18466708076524f856128f15020c
micahnico <micahnicodemus22@gmail.com>
2018-02-10 17:44:52 -0500

    testing stuff

--- app/twilio_logic/twilio_logic.rb
+++ app/twilio_logic/twilio_logic.rb
@@ -6,7 +6,8 @@ class TwilioLogic
   def initialize
     @client ||= Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
     @twilio_number = "+18147534377"
-    @james_number = '+12195774008'
+    #@james_number = '+12195774008'
+    @james_number = '+8145024125'
   end
 
   def send_outgoing_message(recruit, message)
@@ -32,10 +33,10 @@ class TwilioLogic
     recruit = Recruit.find_by(phone_number: from_number)
     if recruit == nil
       # make a GeneralMessage not assigned to any recruit
-      #send_alert_message(from_number)
+      send_alert_message(from_number)
     else
       Message.create(body: message_body, recruit_id: recruit.id, sent_by_recruit: true)
-      #send_alert_message(recruit.name)
+      send_alert_message(recruit.name)
     end
 
   end
