cd8105c3171016d052f00bb58d1215fa47c6dc32
micahnico <micahnicodemus22@gmail.com>
2018-02-10 16:35:17 -0500

    fix issue

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
 	default_scope -> { order(due_date: :desc) }
 
-  belongs_to :recruit, inverse_of: :tasks, dependent: :destroy
+  belongs_to :recruit, inverse_of: :tasks
 end
